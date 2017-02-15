f4b2d4ba2b34112a33c8eadd4d961b8edc444011
TzaraDuchamp <TzaraDuchamp@users.noreply.github.com>
2017-02-15 17:53:00 +0100

    Update 3Pip3.sh

--- OSX/3Pip3.sh
+++ OSX/3Pip3.sh
@@ -2,13 +2,20 @@
 # If file permissions asked, you should open terminal type 'cd' and drag folder in terminal and then enter. 
 # Then type: 'sudo chmod u+x ./3Pip3.sh' and enter This will set the necessary file permissions. 
 # To run the bash script type './3pip3.sh'.
-echo installing via pip3
 
+echo "Installing basic dependencies"
+echo "If some part doesn't install properly, you can run the whole script again or the line that didn't work."
+
+echo installing via pip3
 pip3 install --upgrade pip
 
 # SDK Installation via Pip3
 echo "setting Cozmo up via pip3"
-pip3 install --user 'cozmo[camera]'
+# This command was updated. See: https://forums.anki.com/t/import-cozmo-not-found-working-now/265/43 
+python3 -m pip install --user cozmo[camera]
+# This is the original command
+# pip3 install --user 'cozmo[camera]'
+
 pip3 install --user --upgrade cozmo
 
 
